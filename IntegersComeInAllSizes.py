dd980e80bb0eb4ec32066c40e5d5715f65166ee1
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-02-13 14:29:19 +0530

    Add files via upload

---
+++ IntegersComeInAllSizes.py
@@ -0,0 +1,11 @@
+"""
+Title     : Integers Come In All Sizes
+Subdomain : Math
+Domain    : Python
+"""
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(pow(a, b) + pow(c, d))
